e47851b7691de20ab51f0b457fdc6f44ce6129a2
Alberto696969 <71942953+albertoverrilli@users.noreply.github.com>
2023-06-22 20:05:30 -0400

    Auto Typer.py

--- autotype.py
+++ autotype.py
@@ -1,20 +1,76 @@
 import time
 import pyautogui
-import keyboard
-time.sleep(5)
+import threading
+from tkinter import *
+
+# Define a global flag for controlling typing status
+typing_status = {"running": True, "paused": False}
+
+def countdown(time_sec, countdown_label):
+    for i in range(time_sec, -1, -1):
+        countdown_label.config(text=f"Time until typing starts: {i}s")
+        root.update()
+        time.sleep(1)
+    countdown_label.config(text="")  # Clear countdown label
+
+def pause_resume():
+    global typing_status
+    if typing_status["paused"]:
+        typing_status["paused"] = False
+        pause_resume_button.config(text="Pause")
+        print('Resumed. Typing will start after 3 seconds.')
+        threading.Thread(target=countdown, args=(3, countdown_label)).start()  # Countdown after resume
+    else:
+        typing_status["paused"] = True
+        pause_resume_button.config(text="Resume")
+        print('Paused.')
+
+def stop_typing():
+    global typing_status
+    typing_status["running"] = False
+    print('Stopped.')
+
 def mimic_typing(text, typing_speed=0.05):
+    global typing_status
+    typing_status = {"running": True, "paused": False}  # Reset the state at the start of each new call
+
+    threading.Thread(target=countdown, args=(5, countdown_label)).start()  # Countdown before typing
+    time.sleep(5)
+
     position = 0  # start position
     while position < len(text):
-        if keyboard.is_pressed('['):  # if key 'q' is pressed 
-            print('Paused. Press "]" to resume.')
-            while True:  # wait for 'r' to be pressed
-                if keyboard.is_pressed(']'):  
-                    print('Resumed.')
-                    break
-                time.sleep(0.03)
+        while typing_status["paused"]:
+            time.sleep(0.5)  # if paused, sleep for a while
+        if typing_status["running"] is False:
+            break  # if stopped, break the loop
         pyautogui.typewrite(text[position])
         position += 1
         time.sleep(typing_speed)
 
-text = ""
-mimic_typing(text)
+def send():
+    message = entry.get()
+    if message:
+        threading.Thread(target=mimic_typing, args=(message,)).start()
+        if typing_status["running"] is False:
+            print("Exiting...")
+
+root = Tk()
+root.title("Typing GUI")
+root.geometry("400x200")
+
+entry = Entry(root)
+entry.pack(padx=10, pady=10)
+
+send_button = Button(root, text="Send", command=send)
+send_button.pack(padx=10, pady=10)
+
+pause_resume_button = Button(root, text="Pause", command=pause_resume)
+pause_resume_button.pack(padx=10, pady=10)
+
+stop_button = Button(root, text="Stop", command=stop_typing)
+stop_button.pack(padx=10, pady=10)
+
+countdown_label = Label(root, text="")
+countdown_label.pack()
+
+root.mainloop()
